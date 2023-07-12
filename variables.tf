e2ad6978d287375cfbd56826f948957c20669efa
train4aws <79434869+train4aws@users.noreply.github.com>
2023-07-11 22:52:59 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+  default = "us-east-1"
+  type    = string
+}
+
+variable "ami" {
+  default = "ami-04581fbf744a7d11f"
+  type    = string
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+  type    = string
+}
+
+variable "key_name" {
+  default = "levelupkeypair"
+  type    = string
+}
+
+variable "associate_public_ip_address" {
+  default = "true"
+  type    = bool
+}
+
+variable "jenkins-tag-name" {
+  default = "Jenkins-Server"
+  type    = string
+}
+
+variable "bucket" {
+  default = "jenkins-s3-bucket-ifeanyi-luit20"
+  type    = string
+}
+
+variable "acl" {
+  default = "private"
+  type    = string
+}
