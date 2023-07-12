7fac1202ce77fdc362371d9ab80496c9799928e6
train4aws <79434869+train4aws@users.noreply.github.com>
2023-07-11 23:52:24 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -7,3 +7,12 @@ sudo amazon-linux-extras install java-openjdk11 -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+#!/bin/bash
+cd /tmp
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
