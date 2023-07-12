943a0d35544f23c915c3e8c716032e3658beffc3
train4aws <79434869+train4aws@users.noreply.github.com>
2023-07-11 22:52:27 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  associate_public_ip_address = var.associate_public_ip_address
+  vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
+  user_data                   = file("install_jenkins.sh")
+  iam_instance_profile        = aws_iam_instance_profile.s3-jenkins-profile.name
+
+  tags = {
+    Name = var.jenkins-tag-name
+  }
+}
+
+resource "aws_s3_bucket" "jenkins-s3-bucket" {
+  bucket = var.bucket
+
+  tags = {
+    Name = var.jenkins-tag-name
+  }
+}
+
+resource "aws_s3_bucket_acl" "jenkins-s3-acl" {
+  bucket = aws_s3_bucket.jenkins-s3-bucket.id
+  acl    = var.acl
+}
